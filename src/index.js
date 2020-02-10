b29a347e57322d78b6d0b336aa3de0456ade986c
Oleg Oshurkov <olegosh137@gmail.com>
2020-02-10 22:40:12 +0200

    Added init() calling in index

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import './index.css';
 import './loader.css';
 
 import { addLoader } from './loader';
+import { init } from './init';
 
 addLoader();
+
+// window.addEventListener('load', init, false);
