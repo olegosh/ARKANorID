399141effec15ac9dfc5afca9f9e46ccbd64bafa
Oleg Oshurkov <olegosh137@gmail.com>
2020-02-10 04:42:50 +0200

    Add calcProbability() in utils

--- src/utils.js
+++ src/utils.js
@@ -14,6 +14,41 @@ export function createSounds() {
   }
 }
 
+export function setVol() {
+  for (let i = 0; i < options.vols.length; i += 1) {
+    if (options.vols[i].checked) {
+      options.volume = Number(options.vols[i].dataset.value);
+    }
+  }
+  for (let s in options.sounds) {
+    options.sounds[s].setVolume(options.volume);
+  }
+  playBtnSnd();
+}
+
+export function showAndHide(show, hide) {
+  if (show) {
+    show.style.display = 'block';
+  }
+  if (hide.length) {
+    for (let i = 0; i < hide.length; i += 1) {
+      hide[i].style.display = 'none';
+    }
+  } else {
+    hide.style.display = 'none';
+  }
+}
+
+export function calcProbability(threshold) {
+  let rnd = random(0, threshold);
+  let leftChanceRng = Math.floor(threshold / 2) - Math.floor(threshold / 20);
+  let rightChanceRng = Math.floor(threshold / 2) + Math.floor(threshold / 20);
+  if (rnd >= leftChanceRng && rnd <= rightChanceRng) {
+    return true;
+  }
+  return false;
+}
+
 export function dtr(a) {
   return a * Math.PI / 180;
 }
